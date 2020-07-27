27b83c08e8ff250831c07068b32209525c46e5bf
DukeLimousine <ljd82582@gmail.com>
2020-07-26 23:40:32 -0400

    Adding race table

--- app/models/name.rb
+++ app/models/name.rb
@@ -1,3 +1,4 @@
 class Name < ApplicationRecord
 	belongs_to :name_classification
+	belongs_to :race
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_024654) do
+ActiveRecord::Schema.define(version: 2020_07_27_032434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 2020_07_27_024654) do
     t.integer "gender"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "races", force: :cascade do |t|
+    t.string "name"
   end
 
 end
