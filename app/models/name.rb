bd7012fbd342417ce1d16bee61bc9b0914b0c5ab
DukeLimousine <ljd82582@gmail.com>
2020-07-29 00:04:15 -0400

    races files and name associations

--- app/models/name.rb
+++ app/models/name.rb
@@ -1,4 +1,4 @@
 class Name < ApplicationRecord
-	belongs_to :name_classification
-	belongs_to :race
+	belongs_to :name_classification, :foreign_key => "name_classification_id"
+	belongs_to :race, :foreign_key => "race_id"
 end
