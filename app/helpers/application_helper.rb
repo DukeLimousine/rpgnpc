adf71410fa181af6eaa8c050bfdeb4c1b280d3dd
DukeLimousine <ljd82582@gmail.com>
2021-05-05 17:28:24 -0400

    Stubbing CSV import helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
+require 'csv'
+
 module ApplicationHelper
+  def parse_csv(filepath, modelname)
+  	csv_text = File.read(filepath)
+  	csv = CSV.parse(csv_text, :headers => true)
+
+  	case modelname
+  		when 'name'
+		  	csv.each do |row|
+		  		puts row
+		  	end
+  	else
+  		puts 'options are: name'
+  	end
+  end
 end
