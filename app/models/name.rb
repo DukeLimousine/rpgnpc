4ad96d0afe425a7c5d29543f566e86183a9f205a
DukeLimousine <ljd82582@gmail.com>
2021-07-28 17:32:28 -0400

    Moving random name function to name model

--- app/models/name.rb
+++ app/models/name.rb
@@ -2,4 +2,9 @@ class Name < ApplicationRecord
 	belongs_to :name_classification, :foreign_key => "name_classification_id"
 	belongs_to :race, :foreign_key => "race_id"
 	belongs_to :name_position, :foreign_key => "name_position_id"
+
+  def self.generate_random_name
+	@random_name = Name.where(name_position_id: 1).sample.name.capitalize + ' ' + Name.where(name_position_id: 2).sample.name.capitalize
+  end
+
 end
