372a6e6ab356cd2b512e111a0e8d8aa50760beed
DukeLimousine <ljd82582@gmail.com>
2021-08-03 13:21:36 -0400

    getting name generator form to work

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,3 +1,10 @@
 class ContentController < ApplicationController
+	def generate_name
+	  @name =  Name.new.generate_random_name
 
+      respond_to do |format|
+        format.html { render html: @name}
+        format.json { render json: @name}
+      end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "content#home"
+
+  get 'content/generate_name', to: 'content#generate_name'
 end
