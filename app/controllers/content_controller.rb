d07d88343333ac2c139d60e047f15258abc08e83
DukeLimousine <ljd82582@gmail.com>
2021-08-04 17:46:11 -0400

    taking params for name generator

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,9 +1,23 @@
 class ContentController < ApplicationController
 	def generate_name
-	  @name =  Name.new.generate_random_name
+    args = {}
+    case params[:gender_selector].downcase
+    when "male"
+      args[:gender] = 0
+    when "female"
+      args[:gender] = 1
+    when "neutral"
+      args[:gender] = 2
+    end
+
+    args[:type] = params[:is_epic] ? 2 : 1
+
+    args[:race] = params[:race_selector].to_i
+
+	  @name = Name.new.generate_random_name(args[:type], args[:gender], args[:race])
 
       respond_to do |format|
-        format.html { render html: @name}
+        format.html { render :home }
         format.json { render json: @name}
       end
 	end
